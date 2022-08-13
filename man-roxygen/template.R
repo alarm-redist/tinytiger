f7ed9604ea666007c853bd7d3d87e65428d05414
Cory McCartan <cory.mccartan@gmail.com>
2022-08-13 14:48:43 -0400

    update year

--- man-roxygen/template.R
+++ man-roxygen/template.R
@@ -1,3 +1,3 @@
 #' <%=ifelse(exists("state") && state, "@param state FIPS, postal codes, or full names of states.", "") %>
 #' <%=ifelse(exists("county") && county, "@param county FIPS codes or full names of counties. Optional.", "") %>
-#' <%=ifelse(exists("year") && year, "@param year Integer year. Required. 2012-2020 are currently supported.", "") %>
+#' <%=ifelse(exists("year") && year, "@param year Integer year. Required. 2000 and 2010-2021 are currently supported.", "") %>
