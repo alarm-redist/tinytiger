f25a016fa02fdba7f28765a187a02d1043456d13
Christopher Kenny <ctk44@cornell.edu>
2022-08-13 19:08:27 -0400

    finish extending years

--- R/uac.R
+++ R/uac.R
@@ -12,6 +12,8 @@
 #' tt_uac()
 tt_uac <- function(year = 2021) {
 
+  years_okay(year, 2012:2021)
+
   zip_url <- glue::glue("{base_url(year)}/UAC/tl_{year}_us_uac10.zip")
   target <- paste0("tl_", year, "_us_uac10.shp")
   shp <- tt_download_read(url = zip_url, target_file = target)
--- R/zcta.R
+++ R/zcta.R
@@ -12,8 +12,20 @@
 #' tt_zcta()
 tt_zcta <- function(year = 2021) {
 
-  zip_url <- glue::glue("{base_url(year)}/ZCTA520/tl_{year}_us_zcta520.zip")
-  target <- paste0("tl_", year, "_us_zcta520.shp")
+  years_okay(year, c(2000, 2010, 2012:2021))
+
+  dec_yr <- pad_str(year %% 2000 - (year %% 10))
+  url_adj <- ""
+  if (year >= 2020) {
+    url_adj <- "20"
+  }
+  if (year <= 2010) {
+    url_adj <- paste0("/20", dec_yr)
+    year <- 2010
+  }
+
+  zip_url <- glue::glue("{base_url(year)}/ZCTA5{url_adj}/tl_{year}_us_zcta5{dec_yr}.zip")
+  target <- glue::glue("tl_{year}_us_zcta5{dec_yr}.shp")
   shp <- tt_download_read(url = zip_url, target_file = target)
 
   adj_class(shp)
